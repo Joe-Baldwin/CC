0388c574180a6b97207041d63baa9b2641854598
Joe Baldwin <jbaldw96@gmail.com>
2023-05-23 12:37:46 -0500

    carousel wp

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
 src="node_modules/jquery/dist/jquery.slim.min.js"
 src="node_modules/popper.js/dist/umd/popper.mins.js"
 src="node_modules/bootstrap/dist/js/bootstrap.min.js"
+<!-- Bootstrap Bundle with Popper -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-beta1/js/bootstrap.bundle.min.js" integrity="sha384-pzjw8f+ua7Kw1TIq0v8FqFjcJ6pajs/rfdfs3SO+kD4Ck5BdPtF+to8xM6B5z6W5" crossorigin="anonymous"></script>
